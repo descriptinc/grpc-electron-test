2edf56e597fb47504f3af9a6ae5d0f7dfa7d25d6
Steve Rubin <nerdforlife@gmail.com>
2021-03-09 09:14:55 -0800

    electron 12

--- main.js
+++ main.js
@@ -6,12 +6,15 @@ const grpcTest = require('./grpcTest');
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+app.allowRendererProcessReuse = false;
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800, 
     height: 600,
     webPreferences: {
+      contextIsolation: false,
       nodeIntegration: true,
     },
   })
@@ -40,12 +43,12 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.whenReady().then(createWindow)
 
-// Quit when all windows are closed.
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
@@ -54,11 +57,10 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
